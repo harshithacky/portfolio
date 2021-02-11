bffb072353a2ce0fd5f00fcb674dd549d77d73a9
harshithacky <75036624+harshithacky@users.noreply.github.com>
2021-02-12 01:58:11 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,4 +3,9 @@ $(document).ready(function(){
         $('.menu').toggleClass("active");
         $('.menu-btn span').toggleClass("active");
     });
+
+    $('.menu-items').click(function(){
+        $('.menu').removeClass("active");
+        $('.menu-btn span').toggleClass("active");
+    });
 });
